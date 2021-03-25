841e458f57553cbd2f42e8b7d45d77394a843c76
FilimonovSergey <spark_fil@mail.ru>
2021-03-25 10:32:15 +0300

    add user keyboard

--- start_telebot.py
+++ start_telebot.py
@@ -5,9 +5,16 @@ token = os.getenv("TOKEN")
 bot = telebot.TeleBot(token)
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, 'Я бот. Приятно познакомиться')
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    user_markup.row('/start')
+    user_markup.row('Ассортимент')
+    user_markup.row('Акции')
+    user_markup.row('Отзывы и предложения')
+    user_markup.row('"Возврат и гарантии')
+    bot.send_message(message.from_user.id, 'Добро пожаловать! Я бот компании, приятно познакомиться!',
+                     reply_markup=user_markup)
 
 
 @bot.message_handler(content_types=['text'])
