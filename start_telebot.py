cd2e8e960a6fbd1475ec047c68702e6283da1087
FilimonovSergey <spark_fil@mail.ru>
2021-03-25 10:46:02 +0300

    change user keyboard and dialog with users

--- start_telebot.py
+++ start_telebot.py
@@ -12,7 +12,7 @@ def handle_start(message):
     user_markup.row('Ассортимент')
     user_markup.row('Акции')
     user_markup.row('Отзывы и предложения')
-    user_markup.row('"Возврат и гарантии')
+    user_markup.row('Доставка и оплата')
     bot.send_message(message.from_user.id, 'Добро пожаловать! Я бот компании, приятно познакомиться!',
                      reply_markup=user_markup)
 
@@ -21,8 +21,26 @@ def handle_start(message):
 def get_text_messages(message):
     if message.text.lower() == 'привет':
         bot.send_message(message.from_user.id, 'Привет!!!')
+    elif message.text == 'Ассортимент':
+        bot.send_message(message.from_user.id,
+                         'Вы можете ознакомиться с нашим ассортиментом, перейдя по следующей ссылке:'
+                         'https://melodiasna.ru/')
+    elif message.text == 'Акции':
+        bot.send_message(message.from_user.id,
+                         'Вы можете ознакомиться с действующими акциями, перейдя по следующей ссылке:'
+                         'https://melodiasna.ru/actions/')
+    elif message.text == 'Отзывы и предложения':
+        bot.send_message(message.from_user.id,
+                         'Для того, чтобы ознакомиться с отзывами о продуктах нашей компании, '
+                         'а также оставить свой  отзыв или предложение, перейдите по следующей ссылке:'
+                         'https://melodiasna.ru/o-produkte/reviews/')
+    elif message.text == 'Доставка и оплата':
+        bot.send_message(message.from_user.id,
+                         'Для того, чтобы ознакомиться с информацией о доставке и оплате товаров,'
+                         'перейдите по следующей ссылке:'
+                         'https://melodiasna.ru/dostavka-i-oplata/')
     else:
-        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+        bot.send_message(message.from_user.id, 'Будем рады видеть Вас в наших фирменных магазинах')
 
 
 bot.polling(none_stop=True)
